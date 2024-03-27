27457ad7163786953c437519888b7489bee8c4d4
ent0n29 <stanoantonio29@gmail.com>
2024-03-27 22:10:08 +0100

    script to run automatically all the tests, to obtain the optimized.ll version of all the tests

---
+++ first-assignment/testing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Directory containing the .ll files
+dir="$PATH_TO_YOUR_DIRECTORY/compilers-assignments/first-assignment/tests/"
+
+for file in $dir/*.ll; do
+    base=$(basename $file .ll)
+
+    # run passes
+    opt -p localopts $file -o $dir/$base.optimized.bc
+    llvm-dis $dir/$base.optimized.bc -o $dir/$base\_optimized.ll
+done
+
+rm $dir/*.bc

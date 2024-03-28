7b6bdafdd939556e3498c1cb83802a57642f13a7
ch-fano <christofer.fano8@gmail.com>
2024-03-28 15:38:26 +0100

    Refactored testing folders for better readability

--- first-assignment/testing.sh
+++ first-assignment/testing.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # path to your directory containing the .ll files
-dir="/home/christofer/workspace/GitLLVM/compilers-assignments/first-assignment/tests"
-for file in $dir/*.ll; do
-    if [[ $file =~ _(optimized|expected).ll$ ]]; then continue; fi
-    
+dir="$PATH_TO_YOUR_DIRECTORY/compilers-assignments/first-assignment/testing"
+for file in $dir/tests/*.ll; do
     base=$(basename $file .ll)
     echo -e "\n\nStarting the optimization of $base.ll"
 
     # run passes
-    /home/christofer/workspace/LLVM_17/BUILD/bin/opt -p localopts $file -o $dir/$base.optimized.bc
-    /home/christofer/workspace/LLVM_17/BUILD/bin/llvm-dis $dir/$base.optimized.bc -o $dir/$base\_optimized.ll
+    opt -p localopts $file -o $dir/tests/$base.optimized.bc
+    llvm-dis $dir/tests/$base.optimized.bc -o $dir/optimized/$base\_optimized.ll
 
     echo -e "\nOptimization ended, checking the results"
-    diff <(tail +4 $dir/$base\_optimized.ll) <(cat $dir/$base\_expected.ll)
+    diff <(tail +4 $dir/optimized/$base\_optimized.ll) <(cat $dir/expected/$base\_expected.ll)
     if [[ $? -eq "0" ]]; then echo "Correct results"; fi
 done
 
 # remove bytecode
-rm $dir/*.bc
+rm $dir/tests/*.bc
---
+++ first-assignment/testing/optimized/algebraic_id_optimized.ll
@@ -0,0 +1,12 @@
+; ModuleID = '/home/christofer/workspace/GitLLVM/compilers-assignments/first-assignment/testing/tests/algebraic_id.optimized.bc'
+source_filename = "/home/christofer/workspace/GitLLVM/compilers-assignments/first-assignment/testing/tests/algebraic_id.ll"
+
+define i32 @neutral_sum(i32 noundef %0) {
+  %2 = mul i32 %0, %0
+  ret i32 %2
+}
+
+define i32 @neutral_mul(i32 noundef %0) {
+  %2 = mul i32 %0, %0
+  ret i32 %2
+}

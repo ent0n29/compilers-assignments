428b31514c8d2a7a5c617da921efd2e0dec0e8a1
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-04-06 21:42:20 +0200

    [#15] Split output in blocks

--- first-assignment/testing.sh
+++ first-assignment/testing.sh
@@ -9,18 +9,22 @@ declare -i exit_code=0
 for file in $dir/tests/*.ll; do
 
     base=$(basename $file .ll)
-    echo -e "\n\nStarting the optimization of $base.ll"
+    echo "----- Optimizing $base.ll -----"
 
     # run passes
     opt -p localopts $file -S -o $dir/optimized/$base\_optimized.ll
 
-    echo -e "\nOptimization ended, checking the results"
+    echo "Optimization ended, checking the results"
     diff <(tail +4 $dir/optimized/$base\_optimized.ll) <(cat $dir/expected/$base\_expected.ll)
 
     do_differ=$?
     exit_code+=$do_differ
 
-    if [[ $do_differ -eq "0" ]]; then echo "Correct results"; fi
+    if [[ $do_differ -eq "0" ]];
+      then echo -e "------ PASSED $base.ll ------\n";
+      else echo -e "------ FAILED $base.ll ------\n"
+    fi
+    
 done
 
 exit $exit_code

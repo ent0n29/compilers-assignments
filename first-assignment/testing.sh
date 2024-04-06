2c0dee43838234be4e21622fc20281c4f72bd73f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-04-06 20:23:12 +0200

    fix path & add exit code

--- first-assignment/testing.sh
+++ first-assignment/testing.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # path to your directory containing the .ll files
-dir="$PATH/compilers-assignments/first-assignment/testing"
-for file in $dir/tests/*.ll; do
+DIR="./testing"
+declare -i EXIT_CODE=0
+
+for file in $DIR/tests/*.ll; do
 
     base=$(basename $file .ll)
     echo -e "\n\nStarting the optimization of $base.ll"
 
     # run passes
-    opt -p localopts $file -o $dir/tests/$base.optimized.bc
-    llvm-dis $dir/tests/$base.optimized.bc -o $dir/optimized/$base\_optimized.ll
+    opt -p localopts $file -o $DIR/tests/$base.optimized.bc
+    llvm-dis $DIR/tests/$base.optimized.bc -o $DIR/optimized/$base\_optimized.ll
 
     echo -e "\nOptimization ended, checking the results"
-    diff <(tail +4 $dir/optimized/$base\_optimized.ll) <(cat $dir/expected/$base\_expected.ll)
+    diff <(tail +4 $DIR/optimized/$base\_optimized.ll) <(cat $DIR/expected/$base\_expected.ll)
+
+    EXIT_CODE+=$?
     if [[ $? -eq "0" ]]; then echo "Correct results"; fi
 done
 
 # remove bytecode
-rm $dir/tests/*.bc
+rm $DIR/tests/*.bc
+exit $EXIT_CODE
